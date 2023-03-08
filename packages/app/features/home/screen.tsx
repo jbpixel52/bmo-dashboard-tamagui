7792682c776a0eb2fc41a8b8a47a24a1eee84a46
jbpixel <50649423+jbpixel52@users.noreply.github.com>
2023-03-07 21:59:47 -0800

    playing with tamagui to create a dashboard

---
+++ packages/app/features/home/screen.tsx
@@ -0,0 +1,98 @@
+import { Anchor, Button, CardProps, H1, Input, Paragraph, Separator, Sheet, XStack, YStack } from '@my/ui'
+import { ChevronDown, ChevronUp } from '@tamagui/lucide-icons'
+import React, { useState } from 'react'
+import { useLink } from 'solito/link'
+import { Card, H2 } from 'tamagui' // or @tamagui/card
+
+export function HomeScreen() {
+  const linkProps = useLink({
+    href: '/user/nate',
+  })
+
+  return (
+    <YStack f={1} jc="center" ai="center" p="$4" space>
+      <YStack space="$4" maw={600}>
+        <H1 ta="center">Robots...🌍🍀</H1>
+        <Paragraph ta="center">
+          pee pee poo poo
+        </Paragraph>
+      </YStack>
+      <YStack space="$4" maw={600}>
+        <Card>
+          <Card.Header />
+          <Card.Footer />
+          <AppCard
+            animation="bouncy"
+            size="$4"
+            w={250}
+            h={300}
+            scale={0.9}
+            hoverStyle={{ scale: 0.925 }}
+            pressStyle={{ scale: 0.875 }}
+            _appname='Sonarr'
+          />
+          <Card.Background />
+        </Card>
+      </YStack>
+
+    </YStack>
+  )
+}
+
+interface appCardProps {
+  props: CardProps,
+  _appname: string
+}
+function AppCard(props: appCardProps) {
+  return (
+    <Card theme="dark" elevate size="$4" bordered {...props}>
+      <Card.Header padded>
+        <H2>{props._appname}</H2>
+        <Paragraph theme="alt2">Click to Open</Paragraph>
+      </Card.Header>
+      <Card.Footer padded>
+        <XStack f={1} />
+        <Button br="$10" onClick=()=>{} >Open</Button>
+      </Card.Footer>
+      <Card.Background>
+
+      </Card.Background>
+    </Card>
+  )
+}
+function SheetDemo() {
+  const [ open, setOpen ] = useState(false)
+  const [ position, setPosition ] = useState(0)
+  return (
+    <>
+      <Button
+        size="$6"
+        icon={open ? ChevronDown : ChevronUp}
+        circular
+        onPress={() => setOpen((x) => !x)}
+      />
+      <Sheet
+        modal
+        open={open}
+        onOpenChange={setOpen}
+        snapPoints={[ 80 ]}
+        position={position}
+        onPositionChange={setPosition}
+        dismissOnSnapToBottom
+      >
+        <Sheet.Overlay />
+        <Sheet.Frame ai="center" jc="center">
+          <Sheet.Handle />
+          <Button
+            size="$6"
+            circular
+            icon={ChevronDown}
+            onPress={() => {
+              setOpen(false)
+            }}
+          />
+        </Sheet.Frame>
+      </Sheet>
+    </>
+  )
+}
